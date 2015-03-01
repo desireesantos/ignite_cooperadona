9078bd4b16f8de765c21a5cb8d4a62c7b053e2c4
Miguel Grazziotin <miguelgraz@gmail.com>
2015-03-01 15:17:19 -0300

    improvements

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,15 @@
 class DashboardsController < ApplicationController
   def home
+
+    @PessoaPerfil = Pessoa.all.sample;
+
+    #@Pessoas = Pessoa.where(nome: 'Miguellita');
+
+    @Pessoas = Pessoa.where.not(id: @PessoaPerfil.id)
+
+    @PessoasQueEuPossoEns = Pessoa.all()
+
     render "../views/dashboards/home.html"
+
   end
 end
