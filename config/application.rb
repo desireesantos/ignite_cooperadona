4681ea791748fa5104fc69ea416e762febf46f0b
Desiree Santos <dsantos@thoughtworks.com>
2015-03-01 10:24:01 -0300

    Adding new css file

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in Gemfile-MRI19, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
